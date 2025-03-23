4f1245d578cbd5c73e83b994882d3c30b855b038
Gupta, Puja <Puja.Gupta@netapp.com>
2025-03-23 22:17:59 +0530

    package import

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/puja05netapp/puppy
 
 go 1.24.0
+
+require github.com/puja05netapp/new_pub v0.0.0-20250323125727-d0861c6e11ce // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/puja05netapp/new_pub"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func Bigbark() string {
+	return new_pub.WhenGroupUp(Bark())
+}
+
+func Bigbarks() string {
+	return new_pub.WhenGroupUp(Barks())
 }
